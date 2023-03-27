bff45d0cd62b23ca792ec0d3fc9a7fa23c39330f
electimon <electimon@gmail.com>
2023-03-27 19:10:30 +0800

    sm6250-common: Drop legacy DRM HAL
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I4c7d3864dc0f80c5f11d19b66132f9bd9115ceae

--- common.mk
+++ common.mk
@@ -143,8 +143,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
