d2d86b42f2abcbb76c01d174e857afcb9c0dbe18
COSMIC <rayavanindra@gmail.com>
2023-07-21 16:50:27 +0530

    sm6250-common: Uprev audio hal to 7.1

--- common.mk
+++ common.mk
@@ -31,8 +31,8 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.2-impl
